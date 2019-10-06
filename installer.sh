d0580992e8af697e0198e5d8e5f094756f3853ff
Daniel Baker <kungfootek@gmail.com>
2019-10-06 04:24:41 -0700

    Added something useful to the installerwq

--- installer.sh
+++ installer.sh
@@ -2,7 +2,14 @@
 
 ## Remove .bashrc -> Link to Bashrc in Cli-Foo
 ## Remove .tmux.conf -> Link to tmux.conf in Cli-Foo
-## Create ~/bin folder and copy 
-  - cmatrix
-  - dff
+## Linx ~/Cli-Foo/Bin -> ~/bin folder and copy 
+
+[[ -f ~/.bashrc ]] && rm -f ~/.bashrc
+ln -s ~/Cli-Foo/DotFiles/bashrc ~/.bashrc
+
+[[ -f ~/.tmux.conf ]] && rm -f ~/.tmux.conf
+ln -s ~/Cli-Foo/DotFiles/tmux.conf ~/.tmux.conf
+
+[[ -f ~/bin ]] && rm -rf ~/bin
+ln -s ~/Cli-Foo/Bin ~/bin
 
