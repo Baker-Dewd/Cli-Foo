38bc88ef344610048a9fd156d03fcc038be2eb6a
Daniel Baker <kungfootek@gmail.com>
2023-05-08 14:35:44 -0500

    Added WIP for Compiling dash-cam clips to single move

---
+++ Bin/Compile-Dash-Vidya.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import os
+
+list = []
+
+for files in sorted(os.listdir()):
+    if files.endswith(".MP4"):
+        # Prints only text file present in My Folder
+        list.append(files)
+
+        print(*list, sep = "\n")
+
+
