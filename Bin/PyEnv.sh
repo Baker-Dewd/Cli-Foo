f86a4c8740ebca8f31516af6b7de9b1c339deee1
kungfootek <daniel@kungfootek.net>
2023-08-27 10:18:18 -0500

    Moved the PyEnv stuff into its own space and fixd err when !exists.

--- Bin/PyEnv.sh
+++ Bin/PyEnv.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-pyenv versions | grep '*' | awk '{print $2}'
+
+## Set our PyEnv Variables if we have them. 
+## Otherwise just out put 'Nan'. 
+
+PyEnv="NaN"
+if command -v pyenv
+  then
+    PyEnv-$(pyenv versions | grep '*' | awk '{print $2}')
+    export PYENV_ROOT="$HOME/.pyenv"
+    command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"         
+fi
+export $PyEnv
+echo $PyEnv
